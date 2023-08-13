8a9ce93cfe0b83aa01c6fa9a4b7eab2ffeb571a8
Pierre Morcrette <pierre.morcrette@pm.me>
2023-08-13 22:26:37 +0200

    Copying install.sh from my dotdir repo
     On branch master
     Changes to be committed:
    	new file:   install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+mkdir -p ~/.local/git ~/.config/alacritty
+cd ~/.local/git/  || exit
+nix-env -iA gh git
+gh auth login
+gh repo clone dotdir
+sudo rm /etc/nixos/configuration.nix
+sudo ln -s ~/.local/git/dotdir/configuration.nix /etc/nixos/configuration.nix 
+cp ~/.local/git/dotdir/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -s ~/.local/git/dotdir/org-roam ~/org-roam
+nix-env -e gh git
+sudo nixos-rebuild switch
+emacs dotdir/init.org
+emacs
+systemctl enable --user emacs.service
+systemctl start --user emacs.service
