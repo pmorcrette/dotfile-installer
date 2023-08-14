be0b76a3b8b13d966ee327929ef3498d627bcf5d
Pierre Morcrette <pierre.morcrette@digitalvirgo.com>
2023-08-14 09:41:32 +0200

    Automating org-babel-tangle
     On branch master
     Changes to be committed:
    	modified:   install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,9 @@ cp ~/.local/git/dotdir/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -s ~/.local/git/dotdir/org-roam ~/org-roam
 nix-env -e gh git
 sudo nixos-rebuild switch
-emacs dotdir/init.org
+emacs --batch --eval "(require 'org)" --eval '(org-babel-tangle-file "dotdir/init.org")'
+echo "Wait for emacs to load all package properly then C-x C-c to exit"
+sleep 3
 emacs
 systemctl enable --user emacs.service
 systemctl start --user emacs.service
